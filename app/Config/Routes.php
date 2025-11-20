e3b2ec56d19cbd1f8515ecb6f04f51f135bee7de
Litaapw3ty <jlista9883@gmail.com>
2025-11-20 19:23:23 +0700

    bismillah

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'Beranda::index'); // default ke controller Beranda
+$routes->get('/beranda', 'Beranda::index'); // akses /beranda
+
+// Auth routes
 $routes->get('/register', 'Auth::register');
 $routes->post('/auth/registerProcess', 'Auth::registerProcess');
 $routes->get('/login', 'Auth::login');
+
+$routes->get('/lokasi', 'Lokasi::index');
+$routes->get('lokasi/(:segment)', 'Lokasi::index/$1');
